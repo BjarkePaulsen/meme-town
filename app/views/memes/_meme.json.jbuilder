f4256d43521d911c09dc26a7acc919c31c2d67ca
Bjarke Skot Paulsen <bjarkesp@gmail.com>
2017-07-20 11:26:47 +0000

    First commit

---
+++ app/views/memes/_meme.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! meme, :id, :title, :user_id, :created_at, :updated_at
+json.url meme_url(meme, format: :json)
---
+++ db/migrate/20170720094628_create_memes.rb
@@ -0,0 +1,10 @@
+class CreateMemes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :memes do |t|
+      t.string :title
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
